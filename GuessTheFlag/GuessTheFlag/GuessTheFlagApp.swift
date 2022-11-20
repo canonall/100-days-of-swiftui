fe1a459f2f908378d490755afca06ffb6c832a9f
Can Önal <45923198+canonall@users.noreply.github.com>
2022-11-21 00:32:47 +0300

    add guesstheflag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Can Önal on 24.07.22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
